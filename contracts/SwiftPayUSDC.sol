e1b5e0629e94e5c43d664a555ef1d977e720690c
midesofek <midesofek@gmail.com>
2025-03-04 23:15:17 +0100

    added new contract to reflect new requirements

--- contracts/SwiftPayUSDC.sol
+++ contracts/SwiftPayUSDC.sol
@@ -12,47 +12,116 @@ interface IUSDC {
         uint256 value
     ) external returns (bool);
 
-    function balanceOf(address owner) external view returns (uint);
+    function balanceOf(address owner) external view returns (uint256);
 }
 
 contract SwiftPayUSDC {
     uint256 totalTxns;
-    address public usdcContractAddress;
+    uint256 totalVolume;
+    uint256 public feePercentage; // In basis points (e.g., 50 = 0.5%, 100 = 1%)
+    address public owner;
+    address public feeCollector;
+    address public immutable usdcContractAddress;
     IUSDC usdcContract;
 
-    constructor(address _usdcContractAddress) {
+    event Transfer(
+        address indexed sender,
+        address indexed recipient,
+        uint256 amount
+    );
+    event BatchTransfer(
+        address indexed sender,
+        uint256 noOfRecipients,
+        uint256 totalAmount
+    );
+    event FeeUpdated(uint256 newFeePercentage, address updatedBy);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    constructor(
+        address _usdcContractAddress,
+        address _feeCollector,
+        uint256 _feePercentage
+    ) {
+        require(_feeCollector != address(0), "Invalid collector address");
+        require(_feePercentage <= 100, "Fee cannot exceed 1%");
+
         usdcContractAddress = _usdcContractAddress;
         usdcContract = IUSDC(usdcContractAddress);
+        owner = msg.sender;
+        feeCollector = _feeCollector;
+        feePercentage = _feePercentage;
     }
 
     function transferUsdc(address recipient, uint256 value) external {
-        uint256 balance = getUsdcBalance(msg.sender);
-        if (balance > 0) {
-            require(usdcContract.transferFrom(msg.sender, recipient, value));
-            totalTxns++;
-        }
+        require(usdcContract.transferFrom(msg.sender, recipient, value));
+
+        totalTxns++;
+        totalVolume += value;
+
+        emit Transfer(msg.sender, recipient, value);
     }
 
     function batchTransferUsdc(
         address[] memory recipients,
         uint256[] memory values
     ) external {
+        require(recipients.length == values.length, "Mismatched batch arrays");
+        uint256 totalAmount;
+
         for (uint256 i = 0; i < recipients.length; i++) {
+            uint256 fee = (values[i] * feePercentage) / 10000;
+            uint256 netAmount = values[i] - fee;
+
             require(
-                usdcContract.transferFrom(msg.sender, recipients[i], values[i])
+                usdcContract.transferFrom(msg.sender, recipients[i], netAmount),
+                "Transfer failed"
             );
+
+            if (fee > 0) {
+                require(
+                    usdcContract.transferFrom(msg.sender, feeCollector, fee),
+                    "TransferFailed"
+                );
+            }
+
             totalTxns++;
+            totalAmount += values[i];
+            totalVolume += values[i];
         }
+
+        emit BatchTransfer(msg.sender, recipients.length, totalAmount);
+    }
+
+    /// @notice since solidity doesn't support float/decimal values
+    /// @notice fees are calculated via basis points(bps) 100bps = 1.00% | 50bs = 0.5%
+    function setTransactionFee(uint256 newFee) external onlyOwner {
+        require(newFee <= 100, "Fee cannot exceed 1%");
+        feePercentage = newFee;
+        emit FeeUpdated(newFee, msg.sender);
+    }
+
+    function setFeeCollector(address newCollector) external onlyOwner {
+        require(newCollector != address(0), "Invalid address");
+        feeCollector = newCollector;
     }
 
     /// fn to check usdc balance
-    function getUsdcBalance(address addr) public view returns (uint) {
-        uint balance = usdcContract.balanceOf(addr);
+    function getUsdcBalance(address addr) public view returns (uint256) {
+        uint256 balance = usdcContract.balanceOf(addr);
         return balance;
     }
 
     /// fn to check total txns
     function getTotalTxns() public view returns (uint256) {
         return totalTxns;
+    }
+
+    // fn to check txn volume
+    function getTotalVolume() public view returns (uint256) {
+        return totalVolume;
     }
 }
--- scripts/deployContract.js
+++ scripts/deployContract.js
@@ -7,9 +7,13 @@ const hre = require("hardhat");
 async function main() {
   // USDC BASE-MAINNET: 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913
   // USDC BASE-SEPOLIA: 0x036CbD53842c5426634e7929541eC2318f3dCF7e
-  const usdcContractAddress = "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
+  const usdcContractAddress = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
+  const feeCollector = "0xFC581F1D673269F6d9C5B722f3762622013B753B";
+  const FEE_PERCENTAGE = 100;
   const contract = await hre.ethers.deployContract("SwiftPayUSDC", [
     usdcContractAddress,
+    feeCollector,
+    FEE_PERCENTAGE,
   ]);
   console.log("Deploying contract....");
   await contract.waitForDeployment();
